61e7f0884223700f4ebedd2dfe0ee1c32253e15a
surenderhsr44 <suenderhsr44@gmail.com>
2018-07-31 22:24:56 +0530

    updete two file

--- src/Login.js
+++ src/Login.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 
 class Login extends Component { 
 
- 
  	constructor(props){
  		super(props);
  		this.state={userid:"",userPass:"",massage:""};
  	
- 	this.Handleuserid = (event) => {
-
+	 	this.Handleuserid = (event) => {
  			console.log(event.target.value);
  			this.setState({userid:event.target.value});
  		};
@@ -17,12 +15,15 @@ class Login extends Component {
  			console.log(event.target.value);
  			this.setState({userPass:event.target.value});
  		};
+ 		this.hello = () =>{
+ 			 document.write ("Hello World");
+ 		}
  		this.LoginHandle = () =>{
  			if(this.state.userid==="admin" && this.state.userPass==="admin")
  			{
  				
  				this.setState({massage:" "});
- 				console.log("User id And Password Right");
+ 				this.hello();
  			} else
  			{
  				this.setState({massage:"Wrong userName or Password"});}
@@ -30,13 +31,13 @@ class Login extends Component {
  		}
  	}	render() {
     return (
-      <div className="login">
-      <p>{this.state.massage}</p>
+     <div className="login">
+       <p>{this.state.massage}</p>
        <input type="text" value={this.props.userid} onChange={this.Handleuserid} placeholder="Username"/> <br />
        <input type="password" value={this.props.userPass} onChange={this.Handlepassword} placeholder="Password"/><br />
        <button  onClick={this.LoginHandle}>Login</button>
       
-      </div>
+     </div>
     );
   }
 }
